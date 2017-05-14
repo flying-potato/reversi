951286b09622524f92ceafee4509286a5107e2c4
Chong Li <cl4056@nyu.edu>
2017-05-14 01:47:21 -0400

    border align

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -8,7 +8,7 @@ var urlsToCache = [
   '//yoav-zibin.github.io/TicTacToe/dist/js/everything.min.js',
   '//yoav-zibin.github.io/TicTacToe/dist/css/everything.min.css',
 ];
-var CACHE_NAME = 'cache-v2017-05-13T23:23:57.304Z';
+var CACHE_NAME = 'cache-v2017-05-14T05:46:13.931Z';
 
 self.addEventListener('activate', function(event) {
   event.waitUntil(
